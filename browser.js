d583cd4db9487224811a8e9e92f328309af91bd6
andi-neck <andi.neck@intesso.com>
2015-09-16 10:18:58 +0200

    unify debug string with plain module-name[:script]

--- browser.js
+++ browser.js
@@ -1,7 +1,7 @@
 /**
  * Module dependencies.
  */
-var debug = require('debug')('glint:ckeditor');
+var debug = require('debug')('glint-block-ckeditor');
 var merge = require('utils-merge');
 var load = require('load-script');
 var inherits = require('inherits');
